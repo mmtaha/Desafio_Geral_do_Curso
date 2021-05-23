aed047491c0ee1555823644e5d96ce5b8689018e
danteerich <84204942+danteerich@users.noreply.github.com>
2021-05-23 14:09:45 -0300

    Add files via upload

---
+++ PHP/envia_form.php
@@ -0,0 +1,43 @@
+<?
+
+# alterar a variavel abaixo colocando o seu email
+
+$destinatario = "marcelinoaliny@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+$assunto = $_REQUEST['assunto'];
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - Grupo 2" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:obrigado.html");
+
+ $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+  } else {
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
+  }
+
+
+?>
